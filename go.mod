813c3f0e399d9d53869fcab3e1b5fe3ec0a3829b
Yurii Tereschenko <yu0303@yandex.ru>
2023-03-17 21:04:11 +0500

    done

---
+++ go.mod
@@ -0,0 +1,3 @@
+module learning/module27
+
+go 1.19
---
+++ task.go
@@ -0,0 +1,69 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"strconv"
+	"strings"
+)
+
+type Student struct {
+	name string
+
+	age int
+
+	grade int
+}
+
+func newStudent(name string, age, grade int) Student {
+	return Student{
+		age:   age,
+		name:  name,
+		grade: grade,
+	}
+}
+
+func (s Student) getInfo() {
+	fmt.Println(s.name, s.age, s.grade)
+}
+
+func get(storage map[string]Student) {
+	fmt.Println("Студенты из хранилища:")
+	for _, value := range storage {
+		value.getInfo()
+	}
+}
+
+func put(data string, storage map[string]Student) {
+	dataSplited := strings.Split(data, " ")
+	if len(dataSplited) != 3 {
+		fmt.Println("Incorrect input")
+		return
+	}
+	name := dataSplited[0]
+	age, err := strconv.Atoi(dataSplited[1])
+	if err != nil {
+		fmt.Println("Incorrect input", err)
+		return
+	}
+	grade, err := strconv.Atoi(dataSplited[2])
+	if err != nil {
+		fmt.Println("Incorrect input", err)
+		return
+	}
+
+	storage[name] = newStudent(name, age, grade)
+
+}
+
+func main() {
+	studStorage := make(map[string]Student)
+
+	sc := bufio.NewScanner(os.Stdin)
+	for sc.Scan() {
+		txt := sc.Text()
+		put(string(txt), studStorage)
+	}
+	get(studStorage)
+}
